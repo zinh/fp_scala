53bc17fe0e01d89dddef60d909044bd243bd088d
Vinh Nguyen <xuanvinhct@gmail.com>
2016-05-07 17:32:43 +0700

    update state

--- state/State.scala
+++ state/State.scala
@@ -31,6 +31,19 @@ object RNG{
       (f(a, b), rng2)
     }
 
+  def flatMap[A, B](ra: Rand[A])(f: A => Rand[B]): Rand[B] =
+    rng => {
+      val (a, nextRNG) = ra(rng)
+      f(a)(nextRNG)
+    }
+
+  // map using flatMap
+  def map3[A, B](s: Rand[A])(f: A => B): Rand[B] = flatMap(s)(a => unit(f(a)))
+
+  def map4[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = flatMap(ra)(a => 
+      map(rb)(b => 
+          f(a, b)))
+
   def nonNegativeEven: Rand[Int] = map(nonNegativeRNG)(i => i - i % 2)
 
   def nonNegativeRNG(rng: RNG): (Int, RNG) = {
@@ -40,6 +53,8 @@ object RNG{
     else num
     (positiveNum, nextRNG)
   }
+
+  def nonNegativeInt: Rand[Int] = { rng => nonNegativeRNG(rng) }
 
   def double(rng: RNG): (Double, RNG) = {
     val (num, nextRNG) = nonNegativeRNG(rng)
@@ -79,4 +94,57 @@ object RNG{
     }
     go(count, List())(rng)
   }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] = { rng =>
+    val (i, rng2) = nonNegativeRNG(rng)
+    val mod = i % n
+    if (i + (n - 1) - mod >= 0)
+      (mod, rng2)
+    else
+      nonNegativeLessThan(n)(rng2)
+  }
+
+  def nonNegativeLessThan2(n: Int): Rand[Int] =
+    flatMap(nonNegativeInt){i =>
+      val m = i % n
+      if (i + (n - 1) - m >= 0) unit(m) else nonNegativeLessThan2(n)
+    }
+}
+
+case class State[S, +A](run: S => (A, S)){
+  def unit[A](a: A): State[S, A] = State(s => (a, s))
+
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => {
+    val (a, nextState) = run(s)
+    f(a).run(s)
+  })
+
+  def map[B](f: A => B): State[S, B] = flatMap(a => unit(f(a)))
+
+  def map2[B, C](b: State[S, B])(f: (A, B) => C): State[S, C] = flatMap(
+    a => b.map(b => f(a, b))
+  )
+}
+
+object State{
+  def sequence[S, A](lst: List[State[S, A]]): State[S, List[A]] = {
+    def go(lst: List[State[S, A]], result: List[A], currentState: S): (List[A], S) = lst match {
+      case Nil =>
+        (result, currentState)
+      case hd :: tl =>
+        val (a, nextState) = hd.run(currentState)
+        go(tl, a::result, nextState)
+    }
+    State((s:S) => go(lst, List(), s))
+  }
+  // use foldLeft
+  // def sequence2[S, A](lst: List[State[S, A]]): State[S, List[A]] = State((s: State[S, A]) =>
+  //     lst.foldRight((List(), s))((st, memo) => {
+  //       val currentState = memo._2
+  //       val currentLst = memo._1
+  //       val (a, nextState) = st.run(currentState)
+  //       (a::currentLst, nextState)
+  //     }
+  //     )
+  //   )
 }
