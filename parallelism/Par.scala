181413eb690defab953317ac36998e7214796ec3
Vinh Nguyen <xuanvinhct@gmail.com>
2016-05-21 14:25:59 +0700

    add sequence method

--- parallelism/Par.scala
+++ parallelism/Par.scala
@@ -2,8 +2,8 @@ import java.util.concurrent._
 
 object Par{
   type Par[A] = ExecutorService => Future[A]
-  // Computation immediately results in value of a
   def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+  def lazyUnit[A](a: => A): Par[A] = (es: ExecutorService) => UnitFuture(a)
 
   private case class UnitFuture[A](get: A) extends Future[A] {
     def isDone = true
@@ -11,27 +11,31 @@ object Par{
     def isCancelled = false
     def cancel(evenIfRunning: Boolean): Boolean = false
   }
-  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
-  // mark parallelism
+
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = (es:ExecutorService) => {
+    val af = a(es)
+    val bf = b(es)
+    UnitFuture(f(af.get, bf.get))
+  }
+
   def fork[A](a: => Par[A]): Par[A] =
-    (es: ExecutorService) => es.submit(new Callable[A]{
+    es => es.submit(new Callable[A]{
       def call = a(es).get
     })
-  // spawn and run
-  def run[A](es: ExecutorService)(a: Par[A]): Future[A] = a(es)
-  // combine result of two parallel computation
-  def map2[A, B, C](pa: Par[A], pb: Par[B])(f: (A, B) => C): Par[C] =
-    (es: ExecutorService) => {
-      val af = pa(es)
-      val bf = pb(es)
-      UnitFuture(f(af.get, bf.get))
-    }
 
-  def map[A, B](pa: Par[A])(f: A => B): Par[B] =
-    (es: ExecutorService) => {
-      val af = pa(es)
-      UnitFuture(f(af.get))
-    }
+  def asyncF[A, B](f: A => B): A => Par[B] = (a: A) => lazyUnit(f(a))
 
-  def asyncF[A, B](f: A => B): A => Par[B] = (a: A) => map(Unit(a))(f)
+  def map[A, B](pa: Par[A])(f: A => B): Par[B] = map2(pa, unit(()))((a, _) => f(a))
+
+  def sortPar(lst: Par[List[Int]]): Par[List[Int]] = map(lst)(_.sorted)
+
+  def parMap[A, B](lst: List[A])(f: A => B): Par[List[B]] = {
+    val fbs: List[Par[B]] = lst.map(asyncF(f))
+    sequence(fbs)
+  }
+
+  def sequence[A](lst: List[Par[A]]): Par[List[A]] = lst.foldLeft(unit(List(): List[A]))(
+    (memo: Par[List[A]], pa: Par[A]) => 
+      map2(memo, pa)((lst: List[A], a: A) => (a::lst))
+    )
 }
